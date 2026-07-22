8ab87c66608c0bb6822c36ae2599ea738a42b67f
FelineFantasy <mailsalavata5@gmail.com>
2026-07-22 22:16:32 +0500

    refactor(main): optimize calculation logic and remove code duplication

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,26 @@ fn main() {
     println!("Введите второе число: ");
     let number2: f64 = input().expect("Ошибка ввода числа");
 
-    match op.as_str() {
-        "+" => println!("{} + {} = {}", number1, number2, number1 + number2),
-        "-" => println!("{} - {} = {}", number1, number2, number1 - number2),
-        "*" => println!("{} * {} = {}", number1, number2, number1 * number2),
-        "/" => {
-            if number2 == 0.0 {
-                println!("Ошибка: деление на ноль!");
-            } else {
-                println!("{} / {} = {}", number1, number2, number1 / number2);
-            }
-        }
-        "ฅ+" => println!("{} + {} = {} Лапок", number1, number2, number1 + number2),
-        "ฅ-" => println!("{} - {} = {} Лапок", number1, number2, number1 - number2),
-        "ฅ*" => println!("{} * {} = {} Лапок", number1, number2, number1 * number2),
-        "ฅ/" => {
-            if number2 == 0.0 {
-                println!("Ошибка: деление на ноль!");
-            } else {
-                println!("{} / {} = {} Лапок", number1, number2, number1 / number2);
-            }
-        }
-        _ => println!("Неверный оператор!"),
+    let is_cat_op = op.starts_with('ฅ');
+
+    let clean_op = if is_cat_op { &op[3..] } else { op.as_str() };
+
+    if clean_op == "/" && number2 == 0.0 {
+        println!("Ошибка: деление на ноль!");
+        return;
     }
+
+    let result = match clean_op {
+        "+" => number1 + number2,
+        "-" => number1 - number2,
+        "*" => number1 * number2,
+        "/" => number1 / number2,
+        _ => {
+            println!("Неверный оператор!");
+            return;
+        }
+    };
+
+    let suffix = if is_cat_op { " Лапок" } else { "" };
+    println!("{} {} {} = {}{}", number1, clean_op, number2, result, suffix);
 }
