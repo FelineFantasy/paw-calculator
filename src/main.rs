1a43608cda03bfef0f661b333b5fa5e019f1c8db
FelineFantasy <mailsalavata5@gmail.com>
2026-08-27 23:50:37 +0500

    fix: change operator input from String to char

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,28 @@ fn main() {
     let number1: f64 = input().expect("Ошибка ввода числа");
 
     println!("Введите оператор: ");
-    let op: String = input().expect("Ошибка ввода оператора");
+    let op: char = input()
+        .expect("Ошибка ввода оператора")
+        .chars()
+        .next()
+        .unwrap_or(' ');
 
     println!("Введите второе число: ");
     let number2: f64 = input().expect("Ошибка ввода числа");
 
-    let clean_op = op.strip_prefix("ฅ").unwrap_or(&op);
-    let is_cat_op = clean_op != op;
+    let clean_op = if op == 'ฅ' { '+' } else { op };
+    let is_cat_op = op == 'ฅ';
 
-    if clean_op == "/" && number2.abs() < ZERO_THRESHOLD {
+    if clean_op == '/' && number2.abs() < ZERO_THRESHOLD {
         println!("Ошибка: деление на ноль!");
         return;
     }
 
     let result = match clean_op {
-        "+" => number1 + number2,
-        "-" => number1 - number2,
-        "*" => number1 * number2,
-        "/" => number1 / number2,
+        '+' => number1 + number2,
+        '-' => number1 - number2,
+        '*' => number1 * number2,
+        '/' => number1 / number2,
         _ => {
             println!("Неверный оператор!");
             return;
